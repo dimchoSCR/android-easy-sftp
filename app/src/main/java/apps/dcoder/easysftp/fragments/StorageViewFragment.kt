44402e0d7baeb587c359e0e459a3a57e497b6153
Dimcho Karpachev <>
2020-04-13 12:27:54 +0300

    Set up fragment navigation

---
+++ app/src/main/java/apps/dcoder/easysftp/fragments/StorageViewFragment.kt
@@ -0,0 +1,20 @@
+package apps.dcoder.easysftp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import apps.dcoder.easysftp.R
+
+class StorageViewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_storage_view, container, false)
+    }
+
+}
