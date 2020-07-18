b10a3c04a9d11788ee89ba059906c73a69f11743
Dimcho Karpachev <dimchokarpachev@gmail.com>
2020-07-18 14:11:24 +0300

    Remove storage bottom sheet

--- app/src/main/java/apps/dcoder/easysftp/fragments/StorageListFragment.kt
+++ app/src/main/java/apps/dcoder/easysftp/fragments/StorageListFragment.kt
@@ -12,7 +12,6 @@ import apps.dcoder.easysftp.R
 import apps.dcoder.easysftp.adapters.StorageEntryAdapter
 import apps.dcoder.easysftp.model.status.Status
 import apps.dcoder.easysftp.viewmodels.StorageListViewModel
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.fragment_storage_view.*
 import kotlinx.android.synthetic.main.fragment_storage_view.view.*
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -71,26 +70,8 @@ class StorageListFragment : Fragment() {
     }
 
     private fun initializeListeners() {
-        val sftpBottomSheet = BottomSheetBehavior.from(bsAddSftpServer)
         fabAddSftpServer.setOnClickListener {
-            if (sftpBottomSheet.state == BottomSheetBehavior.STATE_COLLAPSED ||
-                    sftpBottomSheet.state == BottomSheetBehavior.STATE_HIDDEN) {
 
-                sftpBottomSheet.state = BottomSheetBehavior.STATE_EXPANDED
-            }
         }
-
-        sftpBottomSheet.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onSlide(p0: View, p1: Float) { }
-
-            override fun onStateChanged(sheet: View, newState: Int) {
-                when (newState) {
-                    BottomSheetBehavior.STATE_EXPANDED -> fabAddSftpServer.hide()
-                    BottomSheetBehavior.STATE_HIDDEN, BottomSheetBehavior.STATE_COLLAPSED -> fabAddSftpServer.show()
-                    else -> {  }
-                }
-            }
-
-        })
     }
 }
